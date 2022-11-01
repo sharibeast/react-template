50ce2d0b320e157c9c9bac63630fbbf2a4374d23
Sharif Ally <50718667+sharibeast@users.noreply.github.com>
2022-11-01 13:46:48 +0300

    changes

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+function App() {
+  // check this
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img src="vite.svg" alt="sample " />
+        </a>
+        <h1>Sharif</h1>
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
